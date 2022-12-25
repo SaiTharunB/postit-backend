9d018a7979fa11acf541b6c89d839fb162d4c296
SaiTharunB <44254646+SaiTharunB@users.noreply.github.com>
2022-12-25 17:36:55 -0500

    Initial Setup
    setting up repo

---
+++ src/test/java/com/postit/backend/PostitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
