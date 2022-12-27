6cd546a8c9d020d4d9b0897510181625ba0f7e8e
Tharun B <saitarunbodduluru424@gmail.com>
2022-12-27 00:17:01 -0500

    updated props

--- src/main/java/com/postit/backend/controller/CommentController.java
+++ src/main/java/com/postit/backend/controller/CommentController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/comment")
 public class CommentController {
     @Autowired
     CommentService commentService;
