574f6bf6fb8ba597854d20f5be6a9dfd69528dc7
Tharun B <saitarunbodduluru424@gmail.com>
2023-01-03 17:22:35 -0500

    update cors config

--- src/main/java/com/postit/backend/PostItApplication.java
+++ src/main/java/com/postit/backend/PostItApplication.java
@@ -29,7 +29,7 @@ public class PostItApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://129.80.182.24:3000/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://129.80.182.24:3000/","http://localhost:3000/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request-Method",
 				"Access-Control-Request-Headers","user","username","token"));
